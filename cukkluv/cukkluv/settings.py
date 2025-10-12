27693cc42f33b08e003fba5db84ee8b9cf8eec92
pnt2005 <phannthanh2005@gmail.com>
2025-10-12 21:14:09 +0700

    feat(accounts): design login page

--- cukkluv/cukkluv/settings.py
+++ cukkluv/cukkluv/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 LOGIN_URL = '/accounts/login/'
