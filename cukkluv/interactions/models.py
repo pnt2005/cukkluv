7e952b9e55ab4570f1e6790b8fd2b0d8c7d7ce33
hintran24 <hientrannnn24@gmail.com>
2025-10-15 10:59:26 +0700

    design(template) and insert(recipe.models)

--- cukkluv/interactions/models.py
+++ cukkluv/interactions/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
+from recipes.models import Recipe
+from posts.models import Post
 
-# Create your models here.
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='comments', null=True, blank=True)
+    post =models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments', null=True, blank=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Comment by {self.user.username}'
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='likes', null=True, blank=True)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='likes', null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        unique_together = ('user', 'recipe', 'post')
+    def __str__(self):
+        if self.recipe:
+            return f'{self.user.username} liked recipe {self.recipe.title}'
+        return f'{self.user.username} liked post {self.post.id}'
+    
+class Save(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='saves')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='saves')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'recipe')
+
+    def __str__(self):
+        return f'{self.user.username} saved {self.recipe.title}'
+
