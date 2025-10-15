9985c37944f6e6e7448483ee3cc6f294eebd7668
pnt2005 <phannthanh2005@gmail.com>
2025-10-15 11:32:26 +0700

    feat: add recipes models

--- cukkluv/recipes/models.py
+++ cukkluv/recipes/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return f'user_{instance.author.id}/{filename}'
+
 class Recipe(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField()
     ingredients = models.TextField()
     steps = models.TextField()
-    image = models.ImageField(upload_to='recipes/')
+    image = models.ImageField(upload_to=user_directory_path)
     created_at = models.DateTimeField(auto_now_add=True)
     tags = models.ManyToManyField('Tag', through='RecipeTag')
 
