c4bfaab0c3527e7f553393f537269a5c9d63d119
pnt2005 <phannthanh2005@gmail.com>
2025-10-15 11:31:30 +0700

    feat: add app posts

--- cukkluv/posts/models.py
+++ cukkluv/posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+def user_directory_path(instance, filename):
+    # file will be uploaded to MEDIA_ROOT/user_<id>/<filename>
+    return f'user_{instance.author.id}/{filename}'
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    image = models.ImageField(upload_to=user_directory_path)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post by {self.author.username} at {self.created_at}"
+    
--- cukkluv/posts/views.py
+++ cukkluv/posts/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required
+from posts.models import Post
+from posts.forms import PostForm
 
-# Create your views here.
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_list')
+    else:
+        form = PostForm()
+        return render(request, 'posts/create_post.html', {'form': form})
+    
+
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'posts/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'posts/partials/post_detail.html', {'post': post})
+
+
+@login_required
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return JsonResponse({'success': True}, content_type='application/json')
+    return JsonResponse({'success': False}, content_type='application/json', status=400)
