2a311664fbc4422fc047d8ca8543aa0cad118b4f
pnt2005 <phannthanh2005@gmail.com>
2025-10-15 11:30:40 +0700

    feat: add posts to cukkluv/urls

--- cukkluv/cukkluv/urls.py
+++ cukkluv/cukkluv/urls.py
@@ -1,9 +1,15 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from app.views import home_view
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
-    path('accounts/', include('accounts.urls'))
+    path('accounts/', include('accounts.urls')),
+    path('posts/', include('posts.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
