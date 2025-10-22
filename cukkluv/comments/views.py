22d073e201b3ec20abcf3476c1d2ac17e2cdaf1c
pnt2005 <phannthanh2005@gmail.com>
2025-10-22 10:33:24 +0700

    feat: add get, edit, delete comment views

--- cukkluv/comments/views.py
+++ cukkluv/comments/views.py
@@ -4,6 +4,21 @@ from posts.models import Post
 from .models import Comment
 from django.views.decorators.csrf import csrf_exempt
 
+def get_comments(request, post_id):
+    comments = Comment.objects.filter(post_id=post_id).select_related('user').order_by('-created_at')
+    data = [
+        {
+            'id': c.id,
+            'user': c.user.username,
+            'content': c.content,
+            'created_at': c.created_at.strftime('%d-%m-%Y %H:%M'),
+            'is_owner': c.user == request.user
+        }
+        for c in comments
+    ]
+    return JsonResponse({'comments': data})
+
+
 @csrf_exempt
 def add_comment(request, post_id):
     if request.method == 'POST':
@@ -25,4 +40,23 @@ def add_comment(request, post_id):
             'content': comment.content,
             'created_at': comment.created_at.strftime('%d-%m-%Y %H:%M'),
         })
+    return JsonResponse({'success': False})
+
+
+@csrf_exempt
+def edit_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id, user=request.user)
+    if request.method == 'POST':
+        comment.content = request.POST.get('content')
+        comment.save()
+        return JsonResponse({'success': True, 'content': comment.content})
+    return JsonResponse({'success': False})
+    
+
+@csrf_exempt
+def delete_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id, user=request.user)
+    if request.method == 'POST':
+        comment.delete()
+        return JsonResponse({'success': True})
     return JsonResponse({'success': False})
