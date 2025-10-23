3be01496230e5870191c2044f9b46d6e03b756c2
pnt2005 <phannthanh2005@gmail.com>
2025-10-23 12:52:36 +0700

    feat: add accounts api

--- cukkluv/accounts/views.py
+++ cukkluv/accounts/views.py
@@ -1,16 +1,47 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib import messages
-from accounts.forms import RegisterForm
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import IsAuthenticated
+from accounts.serializers import RegisterSerializer, UserSerializer
 
-def register_view(request):
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, 'Login success')
-            return redirect('home')
-    else:
-        form = RegisterForm()
-        return render(request, 'accounts/register.html', {'form': form})
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({'token': token.key}, status=status.HTTP_200_OK)
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        # Xoá token để user đăng xuất
+        request.user.auth_token.delete()
+        return Response({'success': 'Logged out successfully'}, status=status.HTTP_200_OK)
+
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, _ = Token.objects.get_or_create(user=user)
+            return Response({
+                'message': 'User registered successfully.',
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
