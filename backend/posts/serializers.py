f302ab8777aa78a7388a7773280e0fb4597f2570
pnt2005 <phannthanh2005@gmail.com>
2025-10-23 20:05:49 +0700

    feat(be): add like_count and comment_count to get posts

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -4,7 +4,15 @@ from accounts.serializers import UserSerializer
 
 class PostSerializer(serializers.ModelSerializer):
     author = UserSerializer(read_only=True)
+    like_count = serializers.SerializerMethodField()
+    comment_count = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
         fields = '__all__'
+    
+    def get_like_count(self, obj):
+        return obj.likes.count()
+
+    def get_comment_count(self, obj):
+        return obj.comments.count()
