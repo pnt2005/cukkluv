74343a043927aba4bc02ded2dada59b378e4dbe7
pnt2005 <phannthanh2005@gmail.com>
2025-12-18 09:48:54 +0700

    fix

--- backend/accounts/apps.py
+++ backend/accounts/apps.py
@@ -4,6 +4,5 @@ class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
 
-
-def ready(self):
-    import accounts.signals
+    def ready(self):
+        import accounts.signals
--- backend/cukkluv/settings.py
+++ backend/cukkluv/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'accounts',
     'posts',
     'recipes',
     'comments',
@@ -48,6 +47,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'tags',
     'nested_admin',
+    'accounts.apps.AccountsConfig'
 ]
 
 MIDDLEWARE = [
