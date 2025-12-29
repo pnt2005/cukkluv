a0e20e27ec9baf16b7ed9024afcc1aeda918744d
pnt2005 <phannthanh2005@gmail.com>
2025-12-29 15:42:35 +0700

    feat(be): ađ like recipe

--- backend/likes/urls.py
+++ backend/likes/urls.py
@@ -5,6 +5,6 @@ from .views.comment_views import LikeCommentView
 
 urlpatterns = [
     path('posts/<int:post_id>/', LikePostView.as_view(), name='like-post'),
-    path('recipes/<int:post_id>/', LikeRecipeView.as_view(), name='like-recipe'),
+    path('recipes/<int:recipe_id>/', LikeRecipeView.as_view(), name='like-recipe'),
     path('comments/<int:comment_id>/', LikeCommentView.as_view(), name='like-comment'),
 ]
